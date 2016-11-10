e94d426c1dd22e9d6165cf8a4b2102d12d26c86d
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2016-11-10 18:07:06 +0100

    Restart from scratch with a better bot library

--- app.js
+++ app.js
@@ -1,44 +1,27 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var randomstring = require('randomstring');
-var bodyParser = require('body-parser');
+var TelegramBot = require('node-telegram-bot-api');
+var amazon = require('amazon-product-api');
 
-var index = require('./routes/index');
-var message = require('./routes/message');
-
-if (!process.env.TELEGRAM_API_TOKEN){
-	throw new Error('No TELEGRAM_API_TOKEN set in environment variables.');
-}
-
-var client = require('./client');
-
-var webhookToken = process.env.WEBHOOK_TOKEN || randomstring.generate(16);
-client.setWebhook(process.env.BASE_URL + '/' + webhookToken);
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/' + webhookToken, message);
-app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next){
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+var client = amazon.createClient({
+  awsId: process.env.AMAZON_AWS_ID || '',
+  awsSecret: process.env.AMAZON_AWS_SECRET || '',
+  awsTag: process.env.AMAZON_AWS_TAG || ''
 });
 
-// error handlers
-app.use(function(err, req, res, next){
-	res.status(err.status || 500);
-	res.json({
-		message: err.message,
-		error: err
-	});
-});
+var token = process.env.TELEGRAM_API_TOKEN || '';
+var bot = new TelegramBot(token);
 
-module.exports = app;
+bot
+.setWebHook(
+  process.env.BASE_URL + '/' + process.env.WEBHOOK_TOKEN
+);
+
+// Matches /echo [whatever]
+bot
+.onText(
+  /\/echo (.+)/,
+  function (msg, match) {
+    var fromId = msg.from.id;
+    var resp = match[1];
+    bot.sendMessage(fromId, resp);
+  }
+)
--- client.js
+++
@@ -1,4 +0,0 @@
-var TelegramBotClient = require('telegram-bot-client');
-var client = new TelegramBotClient(process.env.TELEGRAM_API_TOKEN);
-
-module.exports = client;
